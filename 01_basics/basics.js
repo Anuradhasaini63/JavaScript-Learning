7b54b0f92d7011d4c8f010f09b12bc77151fc13e
Anuradhasaini63 <anuradha.ui22@gmail.com>
2024-10-16 08:36:08 +0000

    const, let and var

--- 01_basics/basics.js
+++ 01_basics/basics.js
@@ -1,1 +1,9 @@
-console.log("Anuradha");
+// console.log("Anuradha");
+
+const Id = 1234
+let name = "Annu"
+var profile = "Front end developer"
+location = "Noida"
+var state;
+
+console.table([Id, name, profile, location, state])
